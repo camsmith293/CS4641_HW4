084fb4aaf53f7122fe02a73a2f347f81015b95fa
camsmith293 <camsmith293@gmail.com>
2016-04-24 22:04:00 -0400

    Updated expirementers

--- QLearningSolver.py
+++ QLearningSolver.py
@@ -24,7 +24,7 @@ def solve_tictactoe():
 
     val = 0
     for i in range(TicTacToeMDP.STATES):
-        print("State %s: %d" % (str(TicTacToeMDP.convertIndexToTuple(i)), tictactoe.V[i]))
+        # print("State %s: %d" % (str(TicTacToeMDP.convertIndexToTuple(i)), tictactoe.V[i]))
         val += tictactoe.V[i]
 
     print("Value: %d" % val)
