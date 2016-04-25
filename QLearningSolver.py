167000a8de92efda3943ab4322709f80d3eba2d8
camsmith293 <camsmith293@gmail.com>
2016-04-24 21:08:10 -0400

    Shotgun commit

--- QLearningSolver.py
+++ QLearningSolver.py
@@ -8,8 +8,23 @@ def solve_test_taking():
     test_taking.run()
     for index in range(len(TestTakingMDP.STATES)):
         print("%s: %s" % (TestTakingMDP.STATES[index], TestTakingMDP.ACTIONS[test_taking.policy[index]]))
+    print("Time: %d" % test_taking.time)
+
+    val = 0
+    for i, state in enumerate(TestTakingMDP.STATES):
+        val += test_taking.V[i]
+
+    print("Value: %d" % val)
 
 def solve_tictactoe():
     tictactoe.run()
+
+    print("Time: %d" % tictactoe.time)
+
+    val = 0
+    for i in range(TicTacToeMDP.STATES):
+        val += tictactoe.V[i]
+
+    print("Value: %d" % val)
 
 solve_test_taking()
