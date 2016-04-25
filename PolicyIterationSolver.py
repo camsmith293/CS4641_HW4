9c66e309dc747466903fa42935285e509d8de53c
camsmith293 <camsmith293@gmail.com>
2016-04-24 22:11:28 -0400

    Updated expirementers

--- PolicyIterationSolver.py
+++ PolicyIterationSolver.py
@@ -1,15 +1,21 @@
 import TestTakingMDP
 import TicTacToeMDP
 
+import time as _time
+
 test_taking = TestTakingMDP.wrap_to_PI_MDP()
 tictactoe = TicTacToeMDP.wrap_to_PI_MDP()
 
 def solve_test_taking():
+    time = _time.time()
     test_taking.run()
+    time = _time.time() - time
     for index in range(len(TestTakingMDP.STATES)):
         print("%s: %s" % (TestTakingMDP.STATES[index], TestTakingMDP.ACTIONS[test_taking.policy[index]]))
 
-    print("Time: %d" % test_taking.time)
+
+
+    print("Time: %d" % time)
 
     val = 0
     for i, state in enumerate(TestTakingMDP.STATES):
